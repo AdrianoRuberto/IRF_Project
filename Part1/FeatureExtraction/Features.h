c1f4db85d231f29a56738e9e74e1d0252e2e4098
Patrick Kanzler <dev@pkanzler.de>
2017-01-24 14:51:36 +0100

    add feature for area and length of convex hull

--- Part1/FeatureExtraction/Features.h
+++ Part1/FeatureExtraction/Features.h
@@ -130,6 +130,58 @@ public:
 	
 	}
 
+	/*
+	Returns the length and area of a convex hull fitted to the image
+	
+	@param mat the matrix
+	@return vector with length and area of the convex hull
+	*/
+	static vector<double> ConvexHull(const Mat& mat) {
+		//conf
+		int thresh = 100;
+		
+		// blur matrice
+		blur(mat, mat, Size(3, 3));
+
+		Mat threshold_output;
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+
+		/// Detect edges using Threshold
+		threshold(mat, threshold_output, thresh, 255, THRESH_BINARY);
+
+		/// Find contours
+		findContours(threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+		/// Find the convex hull object for each contour
+		vector<vector<Point> >hull(contours.size());
+		for (int i = 0; i < contours.size(); i++)
+		{
+			convexHull(Mat(contours[i]), hull[i], false);
+		}
+
+		int lengthOfHull = 0;
+		int areaOfHull = 0;
+		int currentArea, currentArcLength;
+		for (int i = 0; i< contours.size(); i++) {
+			currentArcLength = arcLength(hull[i], true);
+			if (currentArcLength > lengthOfHull) {
+				lengthOfHull = currentArcLength;
+			}
+			currentArea = contourArea(hull[i], false);
+			if (currentArea > areaOfHull) {
+				areaOfHull = currentArea;
+			}
+		}
+		
+		vector<double> ret;
+
+		ret.push_back(lengthOfHull);
+		ret.push_back(areaOfHull);
+
+		return ret;
+	}
+
 
 
 	Features();
